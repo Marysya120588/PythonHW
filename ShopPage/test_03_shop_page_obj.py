8c7168c9a111d64abe4758199c35e8319c67c18a
Marysya120588 <marysyakontsevich120580@gmail.com>
2025-10-04 19:37:28 +0300

    lesson_03

---
+++ ShopPage/test_03_shop_page_obj.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from ShopPage import ShopPage
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(3)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+def test_shop_total(driver):
+    shop_page = ShopPage(driver)
+    shop_page.open()
+    shop_page.autorisation()
+    shop_page.add_item()
+    shop_page.checkout_click()
+    shop_page.input_data()
+    total = shop_page.get_total()
+    assert total == "Total: $58.29"
+
+
+
+
+
---
+++ calculator/CalculatorPage.py
@@ -0,0 +1,40 @@
+from pycparser.ply.yacc import resultlimit
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+class CalculatorPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        #self.result_locator = (By.ID, "result")
+
+    def open(self):
+        self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    def enter_delay(self):
+        self.driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+        self.driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+
+    def click_button(self):
+        self.driver.find_element(By.XPATH, '//span[text()="7"]').click()
+        self.driver.find_element(By.XPATH, '//span[text()="+"]').click()
+        self.driver.find_element(By.XPATH, '//span[text()="8"]').click()
+        self.driver.find_element(By.XPATH, '//span[text()="="]').click()
+
+    def expectation(self):
+        WebDriverWait(self.driver, 60).until(
+                EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15")
+            )
+
+    def result(self):
+        return self.driver.find_element(By.CSS_SELECTOR, "div.screen").text
+
+    def close_driver(self):
+        self.driver.quit()
+
+
+
